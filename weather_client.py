f6258c05734bb4efbd4f449d4c8e1ff2fde2d39c
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-02-09 17:16:09 -0800

    Update weather_client.py

--- weather_client.py
+++ weather_client.py
@@ -7,7 +7,7 @@ from typing import Dict
 URL = "https://api.openweathermap.org/data/2.5/weather"
 
 # TODO: get an API key from openweathermap.org and fill it in here!
-API_KEY = ""
+API_KEY = "c5e3cfb9d53f9503107ee4b822f9212b"
 
 def get_weather(city) -> Dict:
     res = requests.get(URL, params={"q": city, "appid": API_KEY})
@@ -15,8 +15,16 @@ def get_weather(city) -> Dict:
 
 # TODO: try connecting to a another API! e.g. reddit (https://www.reddit.com/dev/api/)
 
+URL2 = "https://official-joke-api.appspot.com/random_joke"
+
+def random_joke() -> Dict:
+    res2 = requests.get(URL, params={})
+    return res.json()
+    
 def main():
     temp = get_weather("London")
+    print(temp)
+    temp = random_joke()
     print(temp)
 
 if __name__ == "__main__":
