8e29029a9841796bea5a8af7d0edf7e1800dfd4a
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-02-12 12:36:03 -0800

    Update mail_client.py

--- mail_client.py
+++ mail_client.py
@@ -33,24 +33,60 @@ def send_mail(recipient: str, sender: str, subject: str, body: str) -> bool:
 
 def get_inbox(recipient: str) -> None:
     """TODO: fill out this docstring (using the send_mail docstring as a guide)
+    Uses the name of the reciepient to get the mail sent to that recipient
+    
+    Args:
+        recipient (str): The recipient of the mail
+
+    Returns:
+    prints the mail sent to recipient
+    
+    
     """
     response = requests.get(f'{SERVER}/mail/inbox/{recipient}')
     pprint.pprint(response.json())
 
 def get_sent(sender: str) -> None:
     """TODO: fill out this docstring (using the send_mail docstring as a guide)
+        Uses the name of the sender to get the mail sent by that sender
+    
+    Args:
+        sender (str): The sender of the mail
+
+    Returns:
+    prints the mail sent by sender
+    
+    
     """
     response = requests.get(f'{SERVER}/mail/sent/{sender}')
     pprint.pprint(response.json())
 
 def get_mail(mail_id: str) -> None:
     """TODO: fill out this docstring (using the send_mail docstring as a guide)
+    Uses the mail id to get the email you are looking for 
+    
+    Args:
+        mail id (str): The id of the email you are looking for
+
+    Returns:
+    prints the email related to the mail id 
+    
+    
+    
     """
     response = requests.get(f'{SERVER}/mail/{mail_id}')
     pprint.pprint(response.json())
 
 def delete_mail(mail_id: str) -> None:
     """TODO: fill out this docstring (using the send_mail docstring as a guide)
+    Uses the mail id to delete the email you are want
+    
+    Args:
+        mail id (str): The id of the email you are looking for
+
+    Returns:
+    nothing
+    
     """
     response = requests.delete(f'{SERVER}/mail/{mail_id}')
     pprint.pprint(response.json())
